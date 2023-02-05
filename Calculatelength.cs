1c6a850e6387dfe9f86b7425fff1c92cdc0152df
vijay <vijaydurkikar1@gmail.com>
2023-02-05 18:03:45 +0530

    [vijay]Add:LineComparesion

---
+++ Calculatelength.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparesion
+{
+    public class Calculatelength
+    {
+        int lengthX,_lengthY, lenGthsX, lengthsY;
+
+        public Calculatelength(int lengthX, int lengthY, int lengthsX, int lengthsY)
+        {
+            this.lengthX = lengthX;
+            _lengthY = lengthY;
+            lenGthsX = lengthsX;
+            this.lengthsY = lengthsY;
+        }
+
+        public double Calculate()
+        {
+            //Line = sqrt((x2 - x1) ^ 2 + (y2- y1) ^ 2)
+            double length = Math.Sqrt(Math.Pow((lenGthsX - lengthX), 2) + Math.Pow((lengthsY - _lengthY), 2));
+            Console.WriteLine(length);
+            return length;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparesion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculatelength length_One = new Calculatelength(1,2,3,4);
+            double length = length_One.Calculate();
+
+            //UC2
+            Calculatelength length_Two = new Calculatelength(3, 5, 6, 8);
+            double length_ = length_Two.Calculate();
+
+            if (length.Equals(length_))
+                Console.WriteLine("Both line are equal");
+            else
+                Console.WriteLine("Both lines are not equal");
+
+            //UC3
+            if (length.CompareTo(length_) == 0)
+                Console.WriteLine("Equal");
+            else if (length.CompareTo(length_) > 0)
+                Console.WriteLine("Line one is greater");
+            else
+                Console.WriteLine("Line two is greater");
+
+            Console.ReadLine();
+        }
+    }
+}
